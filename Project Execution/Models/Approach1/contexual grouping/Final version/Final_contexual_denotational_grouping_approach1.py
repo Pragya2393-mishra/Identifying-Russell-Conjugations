eab559053f3e3cf1be9a53f8498f9473bdc54075
Pragya Mishra <32080490+Pragya2393-mishra@users.noreply.github.com>
2019-05-15 18:19:11 -0500

    Update Final_contexual_denotational_grouping_approach1.py
    Documentation of code

--- Project Execution/Models/Approach1/contexual grouping/Final version/Final_contexual_denotational_grouping_approach1.py
+++ Project Execution/Models/Approach1/contexual grouping/Final version/Final_contexual_denotational_grouping_approach1.py
@@ -1,140 +1,119 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Feb 17 18:19:34 2019
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 17 18:19:34 2019
 
 @author: pragy
 """
-import gensim
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LogisticRegression
 from sklearn import svm
-import nltk
-from gensim.models.word2vec import Word2Vec
 from gensim.models import KeyedVectors
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
-import imblearn
 from imblearn.over_sampling import RandomOverSampler
-from sklearn.datasets import make_classification
 import warnings
 warnings.filterwarnings('ignore')
-from random import shuffle
-import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA 
-from sklearn import preprocessing
 from sklearn.model_selection import cross_val_score
-from sklearn.naive_bayes import GaussianNB
-from imblearn.pipeline import Pipeline
 from sklearn.model_selection import cross_val_predict, cross_val_score
-
-# word2vec model
-path1 = 'F:/somebody gonna get hurt/Github/Model/GoogleNews-vectors-negative300-normed.bin'
-model = KeyedVectors.load(path1, mmap='r')
-model.syn0norm = model.syn0  # prevent recalc of normed vectors
-model.most_similar('stuff')
-type(model['whistleblower'])
-
-#Data_version11 was created combining the above 2 steps
-data = pd.read_csv('F:/somebody gonna get hurt/Github/Model/data/datav19_v3.csv')
-
-#generating difference vector
-l =[]
-for index, row in data.iterrows():
-    print(row['x1'], row['x2']) 
-    try:
-        l.append(model[row['x1']]-model[row['x2']])
-    except:
-        l.append('not in dictionary')
-data['res_vector']=l
-#deleting the rows where difference vector could not be generated (because the word was not in vocabulary)
-data = data[data.res_vector != 'not in dictionary']
-data=data.reset_index(drop=True)
-data[data.y ==1].count() #372
-data[data.y ==0].count() #4398
-
-#Preparing data for visualization
-data1=data
-y=data1.y
-data1.describe()
-y.describe()
-data1 = data1.drop("x1", axis=1)
-data2=data1.drop("x2", axis=1)
-data2=data2.drop("y", axis=1)
-data2=data2.reset_index(drop=True)
-xxx=pd.DataFrame(data2['res_vector'].values.tolist())
-data3=xxx
-X=data3
-                                          
-
-oversamp = RandomOverSampler(random_state=12)   
-classifier = svm.SVC(kernel='rbf', gamma=0.3)                               
-def model_cv_predict(classifier, feature_vector_train, labels):
-    estimator = Pipeline([("oversampler", oversamp),("classifier", classifier)])
-    predictions = cross_val_predict(estimator, feature_vector_train, labels, cv=10)
-    f1 = cross_val_score(estimator, feature_vector_train, labels, cv=10,scoring='f1_macro')
-    accuracy = cross_val_score(estimator, feature_vector_train, labels, cv=10)
-    precision = cross_val_score(estimator, feature_vector_train, labels, cv=10,scoring='precision_macro')
-    recall = cross_val_score(estimator, feature_vector_train, labels, cv=10,scoring='recall_macro')
-    return f1,accuracy,precision,recall
-
-f1,accuracy,precision, recall= model_cv_predict(classifier, X,y)
-
-f1_score_cv=np.mean(f1) #0.7914249436945597
-accuracy_score_cv = np.mean(accuracy) #0.9407896621673318
-precision_Score_cv = np.mean(precision) #0.9260995220105606
-recall_score_cv= np.mean(recall) #0.7411924155318232
-
-
-#Performing oversampling to offset the effect of negative bias
-ros = RandomOverSampler(random_state=0)
-x_resampled, y_resampled = ros.fit_resample(X, y)
-#New predictors(x1) and predicted(y1)
-x1=list(x_resampled)
-y1=list(y_resampled)
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
+from imblearn.pipeline import Pipeline
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
 from collections import Counter
-print(sorted(Counter(y_resampled).items()))
 
-#Building SVM model and performing cross-validation
-clf = svm.SVC(kernel='rbf', gamma=0.3) 
-clf.fit(x1,y1)
+def main():
 
-#Predicting results on test df
-
-test1 = pd.read_csv('F:/somebody gonna get hurt/Github/Model/Test files/test9_20.csv')
-l =[]
-for index, row in test1.iterrows():
-    print(row['x1'], row['x2']) 
-    try:
-        l.append(model[row['x1']]-model[row['x2']])
-    except:
-        l.append('not in dictionary')
-test1['res_vector']=l 
-
-test1 = test1[test1.res_vector != 'not in dictionary']
-test1=test1.reset_index(drop=True)
-test11=test1
-y=test11.y
-test11.describe()
-y.describe()
-test11 = test11.drop("x1", axis=1)
-test12=test11.drop("x2", axis=1)
-test12=test12.drop("y", axis=1)
-xxx=pd.DataFrame(test12['res_vector'].values.tolist())
-test13=xxx
-X_test=test13
-
-
-y_pred=clf.predict(X_test)
-test1['y-pred']=y_pred
-accuracy= accuracy_score(test1['y'],test1['y-pred']) #0.7272727272727273
-f1=f1_score(test1['y'],test1['y-pred']) #0.7000000000000001
-precision= precision_score(test1['y'],test1['y-pred']) #0.56
-recall=recall_score(test1['y'],test1['y-pred']) #0.9333333333333333
-confusion_matrix(test1['y'],test1['y-pred'])
-#array([[18, 11],
-#       [ 1, 14]], dtype=int64)
-
-test1=test1.drop('res_vector',axis=1)
-
-test1.to_csv('F:/somebody gonna get hurt/Github/Model/Models/Russell Conjugation grouping/Final version/Contexual_grouping_Test18_data19-20_svmr_trying.csv')
+    ###################################################################################################################################
+    #Reading Data
+    def import_data():
+        root = tk.Tk()
+        name = askopenfilename(title = "Select file",filetypes = (("csv",["*.csv",'*.txt']),("all files","*.*")))
+        root.withdraw()        
+        df= pd.read_csv(name, sep=',')
+        return df
+    
+    data=import_data() #import data_sentiment_classification.csv
+    test=import_data() #import Validation set
+    ###################################################################################################################################
+    
+    # word2vec model
+    path1 = 'F:/somebody gonna get hurt/Github/Project Execution/GoogleNews-vectors-negative300-normed.bin'
+    model = KeyedVectors.load(path1, mmap='r')
+    model.syn0norm = model.syn0  # prevent recalc of normed vectors
+    model.most_similar('stuff')
+    
+    ###################################################################################################################################
+    
+    #Data prep
+    #creating difference vectors
+    def diff_vector(data):
+        l =[]
+        for index, row in data.iterrows():
+            #print(row['x1'], row['x2']) 
+            try:
+                l.append(model[row['x1']]-model[row['x2']])
+            except:
+                l.append('not in dictionary')
+        data['res_vector']=l
+        data = data[data.res_vector != 'not in dictionary']
+        data=data.reset_index(drop=True)
+        return data    
+    
+    data= diff_vector(data)
+    test =diff_vector(test)
+    ##################################################################################################################################
+    
+    
+    #Preparing data for visualization
+    data_original=data
+    y=data.y
+    X=pd.DataFrame(data['res_vector'].values.tolist())                                            
+    
+    oversamp = RandomOverSampler(random_state=12)   
+    classifier = svm.SVC(kernel='rbf', gamma=0.3)                               
+    def model_cv_predict(classifier, feature_vector_train, labels):
+        estimator = Pipeline([("oversampler", oversamp),("classifier", classifier)])
+        predictions = cross_val_predict(estimator, feature_vector_train, labels, cv=10)
+        f1 = cross_val_score(estimator, feature_vector_train, labels, cv=10,scoring='f1_macro')
+        accuracy = cross_val_score(estimator, feature_vector_train, labels, cv=10)
+        precision = cross_val_score(estimator, feature_vector_train, labels, cv=10,scoring='precision_macro')
+        recall = cross_val_score(estimator, feature_vector_train, labels, cv=10,scoring='recall_macro')
+        return f1,accuracy,precision,recall
+    
+    f1,accuracy,precision, recall= model_cv_predict(classifier, X,y)
+    
+    f1_score_cv=np.mean(f1) #0.7914249436945597
+    accuracy_score_cv = np.mean(accuracy) #0.9407896621673318
+    precision_Score_cv = np.mean(precision) #0.9260995220105606
+    recall_score_cv= np.mean(recall) #0.7411924155318232
+    
+    
+    #Building Classifier
+    ros = RandomOverSampler(random_state=0)
+    x_resampled, y_resampled = ros.fit_resample(X, y)
+    x1=list(x_resampled)
+    y1=list(y_resampled)
+    print(sorted(Counter(y_resampled).items()))
+    
+    #Building SVM model and performing cross-validation
+    clf = svm.SVC(kernel='rbf', gamma=0.3) 
+    clf.fit(x1,y1)
+    
+    #Testing Classifier on Validation set
+    test_original=test
+    X_test=pd.DataFrame(test['res_vector'].values.tolist()) 
+    
+    y_pred=clf.predict(X_test)
+    test['y-pred']=y_pred
+    accuracy= accuracy_score(test['y'],test['y-pred']) #0.7272727272727273
+    f1=f1_score(test['y'],test['y-pred']) #0.7000000000000001
+    precision= precision_score(test['y'],test['y-pred']) #0.56
+    recall=recall_score(test['y'],test['y-pred']) #0.9333333333333333
+    confusion_matrix(test['y'],test['y-pred'])
+    #array([[18, 11],
+    #       [ 1, 14]], dtype=int64)
+    
+    
+if __name__ == "__main__":
+    main()
